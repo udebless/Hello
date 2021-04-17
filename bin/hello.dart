f1b761af24463498525bf633fe653c6ef15774b6
udebless <udechukwublessed@gmail.com>
2021-04-17 08:47:23 +0100

    I had to put all the codes in a functions for easy accessibility

--- bin/hello.dart
+++ bin/hello.dart
@@ -1,14 +1,37 @@
+import 'dart:io';
+import 'dart:math';
+
+String prompt(String prompText) {
+  print(prompText);
+  String input = stdin.readLineSync();
+  return input;
+}
+
 void main() {
-  int calcSum(int con) { // function with arguement name con
-    var sum = 0; var input = con; // initialize variables
-    while(input > 0){ // run the function as long as the input is greater than zero
-      sum += input -1;  // increase sum withe the value of input - 1
-      input -=1; // reduce the value of input by - 1
+  int calcSum(int con) {
+    // function with arguement name con
+    var sum = 0;
+    var input = con; // initialize variables
+    while (input > 0) {
+      // run the function as long as the input is greater than zero
+      sum += input - 1; // increase sum withe the value of input - 1
+      input -= 1; // reduce the value of input by - 1
     }
-return sum; // return the overall summ
+    return sum; // return the overall summ
   }
- 
-  print(calcSum(50)); // print the sum
+
+  void wileLoop() {
+    List<String> friends = ["stan", "you", "me", "dog", "for"];
+
+    int i = 0;
+    while (i < friends.length - 1) {
+      print(friends[i]);
+      i++;
+      print(friends[i]);
+    }
+  }
+  void sayHi(String username, int age) {
+    print("hi ${username},you are ${age}");
+   }
 
 }
-//write a function that takes argument of type int and returns the sum of all the numbers before it.
